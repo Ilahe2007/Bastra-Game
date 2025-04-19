35b1ef81a83794337ea3ecd4c78ea01de8e41426
Ilaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2025-04-19 14:17:33 +0400

    Create exercise_3.h

---
+++ exercise_3.h
@@ -0,0 +1,12 @@
+typedef unsigned short CARD;
+
+void printCardPart(CARD card, int part);
+
+void showCard(CARD card);
+
+void showCards(CARD cards[], int length);
+
+#include <stdio.h>
+#include "exercise-3-show-cards.c"
+#include <stdlib.h>
+#include <time.h>
